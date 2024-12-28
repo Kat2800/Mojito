684445a16d08267728d5a4fc202e3c83fe9a550d
rickyfili10 <riccardofilieri@protonmail.com>
2024-12-28 19:25:18 +0100

    Update mojstd.py

--- src/libs/mojstd.py
+++ src/libs/mojstd.py
@@ -1,12 +1,13 @@
 import RPi.GPIO as GPIO
 from PIL import Image, ImageDraw, ImageFont
-import os 
+import os
 import sys
 import time
 import subprocess
 import psutil
 import json
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'libs')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/kali/Mojito/src/libs')))
+
 import LCD_1in44
 # Network settings
 BROADCAST_IP = '<broadcast>'
