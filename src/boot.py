c0b36324b24d4632bc0e0265116e4ca04c8cb5de
rickyfili10 <riccardofilieri@protonmail.com>
2025-01-03 11:33:24 +0100

    Update boot.py

--- src/boot.py
+++ src/boot.py
@@ -11,13 +11,7 @@ from libs.mojstd import *
 from libs.updater import *
 import os
 import glob
-
-def mon0():
-        os.system("sudo iw wlan0 interface add mon0 type monitor")
-        os.system("sudo airmon-ng start mon0")
-
-
-mon0()        
+     
 plugins = glob.glob('plugins/boot/*.py')
 
 for file in plugins:
