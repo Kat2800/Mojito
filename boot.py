213933acae6faf64e0e42809d8560a749332d452
rickyfili10 <riccardofilieri@protonmail.com>
2025-02-24 20:56:52 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -12,7 +12,7 @@ from libs.updater import *
 import os
 import glob
 import time
-clear()
+
 show_image("images/logo.png", exit_event=lambda: time.sleep(1) or True)
 plugins = glob.glob('plugins/boot/*.py')
 
